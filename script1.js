b0c351c80ea33a66d6e092e020e73783149a5b1d
hiredcenter <hi.red.center1963@gmail.com>
2018-09-18 23:26:09 +0900

    作業途中(180816)

--- script1.js
+++ script1.js
@@ -12,38 +12,46 @@ $(function(){
 
 	function checkISBN(isbn){
 		console.log(isbn);
-		//※ハイフンがある場合の判定追加
-		//var gAAAABBBB = /^(978|979)-?[014]-?\d{1,7}-?\d{1,7}-?\d{1}$/; //
 
-		//入力値にハイフンがある場合除却
-		var splitHyphenIsbn = isbn.replace(/-/g, ''); // /-/g全てのハイフン
+		//ハイフンがある場合
+		if (isbn.match(/-/)){
+			return isbn.match(/-/);
+			//※ハイフンが指定箇所にすべてある・なしの判定
+			var regExpHyphen = /^ISBN?978-[014]-\d{1,7}-\d{1,7}-\d{1}$/;
+			if(regExpHyphen.test(isbn)){
+				//何もしない
+			} else {
+				return false;
+			}
+		}
+		//入力値にISBN,ハイフンがある場合除却
+		var splitHyphenIsbn = isbn.replace(/ISBN|-/g, ''); // ISBN,ハイフンをすべて除却
 		console.log(splitHyphenIsbn);
 
 		//Cの文字列分割
 		var strC = splitHyphenIsbn.split('');//数値のままでは分割できないらしいのでNumber()の前に記述
-		console.log(strC);
 		//配列Cの値(文字列)を数値に変換
 		var numC = strC.map(function (value){
 			return Number(value);
 		});
-		console.log(numC);
-
-		//Cのチェックディジット計算→forループできる　 1,3の判定は余り算%
+		//Cのチェックディジット計算→forループできる 1,3の判定は余り算%
 		var checkDigitC = 10 - ((numC[0]*1 + numC[1]*3 + numC[2]*1 + numC[3]*3 + numC[4]*1 + numC[5]*3 + numC[6]*1 + numC[7]*3 + numC[8]*1 + numC[9]*3 + numC[10]*1 + numC[11]*3) % 10);
 		console.log(checkDigitC);
+		//値が10の場合、0にする
 		if(checkDigitC == 10) {
 			checkDigitC = 0;
 		}
-		if(checkDigitC !== numC[12]) {
-			//ダメなときの処理を追加
+
+		//checkDigitC とnumC[12]が一致しないなら、falseを返す!
+		if (checkDigitC !== numC[12]) {
+			return false;
 		}
-		console.log(checkDigitC);
 
 		//入力値(文字列)を数値に変換
 		var numIsbn = Number(splitHyphenIsbn);
+		//ハイフン無しでの数値判定
+		var regExpNum = /^978[014]\d{8}\d{1}$/; //abの桁数の判定→最後に
 
-		var gAAAABBBB = /^978[014](\d{1,7}\d{1,7}){8}\d{1}$/; //  ハイフンある場合-?→ハイフンがあってもなくても
-
-		return gAAAABBBB.test(numIsbn); //.test()→文字列の有無を調べる
+		return regExpNum.test(numIsbn); //.test()→文字列の有無を調べる
 	}
 });
