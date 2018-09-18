9092ad9c5003e4820adce9697032fc3feab3b808
hiredcenter <hi.red.center1963@gmail.com>
2018-09-18 23:41:01 +0900

    課題3-2：作業途中(180821)

--- script2.js
+++ script2.js
@@ -1,130 +1,101 @@
-$(function(){
+$(function() {
 
-	$('input[name="check"]').on('click', function(){
+	$('input[name="check"]').on('click', function() {
 		var isbn = $('input[name="isbn"]').val();
 		var res = checkISBN(isbn);
-		if(res){
+		if (res) {
 			$('.result').removeClass('error').text('正しいISBNコードです。');
 			$('.resultType').text(res['type']);
 			$('.resultCountry').text(res['country']);
-		}else{
+		} else {
 			$('.result').addClass('error').text('不正なISBNコードです。');
 			$('.resultType').text('');
 			$('.resultCountry').text('');
 		}
 	});
 
-	function checkISBN(isbn){
-		console.log(isbn);
-		//・正しいコードの場合、resultTypeにISBNの種類が表示（ISBN-10／ISBN-13）
-		//・正しいコードの場合、resultCountryに言語圏が表示（英語圏／日本語圏）
+	function checkISBN(isbn) {
+		//入力値にISBN,ハイフンがある場合除却
+		var splitHyphenIsbn = isbn.replace(/ISBN|-/gi, ''); // ISBN,ハイフンをすべて除却
 
-		//記号除却
-		var regExpHyphen = /^(ISBN)?\d+-?\d-?\d+-?\d+-?\d$/i;
-		if(!regExpHyphen.test(isbn)){
+		//ISBN、ハイフン表記の判定
+		if (!/^(ISBN)?\d+-?\d-?\d+-?\d+-?\d$/i.test(isbn)) {
 			return false;
+			console.log('false');
 		}
+
+		//ISBN-13の場合
+		if (/^978[014]\d{8}\d{1}$/.test(splitHyphenIsbn)) { //表記判定
+			//文字列分割し、配列の値(文字列)を数値に変換 ※数値のままでは分割できないのでNumber()の前に記述
+			var numISBN13 = splitHyphenIsbn.split('').map(function(value) {
+				return Number(value);
+			});
+
+			//Cのチェックディジット計算→forループできる 1,3の判定は余り算%
+			var result13 = 0;
+			var numD;
+			for (var i = 0; i < 12; i++) {
+				if (i % 2 !== 0) { //iが奇数番のときはnumD=1
+					numD = 1;
+				} else if (i % 2 === 0) { //iが偶数番のときはnumD=3
+					numD = 3;
+				}
+				result13 += numISBN13[i] * numD;
+			}
+			var checkDigitISBN13 = 10 - result13 % 10;
+
+			//値が10の場合、0にする
+			if (checkDigitISBN13 === 10) {
+				checkDigitISBN13 = 0;
+			}
+			//checkDigitISBN13 とnumISBN13[12]が一致しないなら、falseを返す!
+			if (checkDigitISBN13 !== numISBN13[12]) {
+				return false;
+			}
+			//4文字目から言語圏判定(0,1:英語、4:日本)
+			// if ( splitHyphenIsbn[3] === 0 || splitHyphenIsbn[3] === 1) {
+			// 	// res['country'] = '英語圏';
+			// 	this.country = '英語圏';
+			// } else if (splitHyphenIsbn[3] === 4) {
+			// 	// res['country'] = '日本語圏';
+			// 	this.country = '日本語圏';
+			// }
+			return true;
 
 		//ISBN-10の場合
-		var regExpHyphenTen = /^(ISBN)?\d+-?\d+-?\d+-?\d$/i;
+		} else if (/^[014]\d{8}\d{1}$/.test(isbn)) {
+			//文字列分割し、配列の値(文字列)を数値に変換 ※数値のままでは分割できないのでNumber()の前に記述
+			var numISBN10 = splitHyphenIsbn.split('').map(function(value) {
+				return Number(value);
+			});
 
-		if(!regExpHyphenTen.test(isbn)){
-			return false;
-		}
-		//入力値にISBN,ハイフンがある場合除却
-		var splitHyphenIsbn = isbn.replace(/ISBN|-/gi, ''); // ISBN,ハイフンをすべて除却
-		console.log(splitHyphenIsbn);
-		var regExpHyphenTen2 = /^[014]\d{8}\d{1}$/; // 言語圏の判定、桁数の判定
-		if(!regExpHyphenTen2.test(splitHyphenIsbn)){
-			return false;
-		}
-
-		//Cの文字列分割
-		var strC = splitHyphenIsbn.split('');//数値のままでは分割できないらしいのでNumber()の前に記述
-		//配列Cの値(文字列)を数値に変換
-		var numC = strC.map(function (value){
-			return Number(value);
-		});
-
-		//Cのチェックディジット計算→forループできる 1,3の判定は余り算%
-		var result = 0;
-		var numD;
-		for (var j = 0; j < 12; j++) {
-			if(i % 2 !== 0){ //iが奇数番のときはnumD=1
-				numD = 1;
-			}else if(j % 2 === 0){ //iが偶数番のときはnumD=3
-				numD = 3;
+			//Cのチェックディジット計算→forループできる 1,3の判定は余り算%
+			var result10 = 0;
+			for (var j = 0; j < 9; j++) {
+				result10 += numISBN10[j] * (10 - j);
 			}
-			result+= numC[j]*numD;
-			//console.log(result);
-		}
-		//console.log(result);
-		var checkDigitM11 = 10 - result % 10;
+			var checkDigitISBN10 = 11 - result10 % 11;
 
-		//値が10の場合、0にする
-		if(checkDigitM11 === 10) {
-			checkDigitM11 = 0;
-		}
-
-		//checkDigitM11 とnumC[12]が一致しないなら、falseを返す!
-		if (checkDigitM11 !== numC[12]) {
-			return false;
-		}
-		//初めの1文字目から言語圏判定(0,1:英語、4:日本)
-		if ( splitHyphenIsbn[0] == 0 || splitHyphenIsbn[0] == 4) {
-			//res['type'] = '';
-		}
-		return true;
-
-
-
-		//ISBN13の判定
-		//※ハイフン有無の判定
-		var regExpHyphen = /^(ISBN)?\d+-?\d-?\d+-?\d+-?\d$/i;
-		if(!regExpHyphen.test(isbn)){
-			return false;
-		}
-		//入力値にISBN,ハイフンがある場合除却
-		var splitHyphenIsbn = isbn.replace(/ISBN|-/gi, ''); // ISBN,ハイフンをすべて除却
-		console.log(splitHyphenIsbn);
-		var regExpHyphen2 = /^978[014]\d{8}\d{1}$/;　
-		if(!regExpHyphen2.test(splitHyphenIsbn)){
-			return false;
-		}
-
-		// //Cの文字列分割
-		var strC = splitHyphenIsbn.split('');//数値のままでは分割できないらしいのでNumber()の前に記述
-		//配列Cの値(文字列)を数値に変換
-		var numC = strC.map(function (value){
-			return Number(value);
-		});
-
-		//Cのチェックディジット計算→forループできる 1,3の判定は余り算%
-		var result = 0;
-		var numD;
-		for (var i = 0; i < 12; i++) {
-			if(i % 2 !== 0){ //iが奇数番のときはnumD=1
-				numD = 1;
-			}else if(i % 2 === 0){ //iが偶数番のときはnumD=3
-				numD = 3;
+			//値が10の場合はX、11の場合は0
+			if (checkDigitISBN10 === 10) {
+				checkDigitISBN10 = X; //これダメ、文字列をどう扱う？？ 文字列結合？
+			} else if (checkDigitISBN10 === 11) {
+				checkDigitISBN10 = 0;
 			}
-			result+= numC[i]*numD;
-			//console.log(result);
-		}
-		//console.log(result);
-		var checkDigitC = 10 - result % 10;
-		//var resultCC = numC[0]*1 + numC[1]*3 + numC[2]*1 + numC[3]*3 + numC[4]*1 + numC[5]*3 + numC[6]*1 + numC[7]*3 + numC[8]*1 + numC[9]*3 + numC[10]*1 + numC[11]*3;
-		//var checkDigitCC = 10 - resultCC % 10;
-		//console.log(checkDigitC+checkDigitCC);
 
-		//値が10の場合、0にする
-		if(checkDigitC === 10) {
-			checkDigitC = 0;
-		}
-		//checkDigitC とnumC[12]が一致しないなら、falseを返す!
-		if (checkDigitC !== numC[12]) {
+			//checkDigitISBN13 とnumISBN10[12]が一致しないなら、falseを返す!
+			if (checkDigitISBN10 !== numISBN10[10]) {
+				return false;
+			}
+			//1文字目から言語圏判定(0,1:英語、4:日本)
+			// if ( splitHyphenIsbn[0] == 0 || splitHyphenIsbn[0] == 4) {
+			// 	//res['type'] = '';
+			// }
+			return true;
+
+		//その他のパターン
+		} else {
 			return false;
 		}
-		return true;
 	}
 });
