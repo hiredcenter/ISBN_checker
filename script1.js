b5a025d8a90d46376becb76cd1f8c0e26f9345fc
hiredcenter <hi.red.center1963@gmail.com>
2018-10-04 03:56:22 +0900

    課題3-1：再FB 課題3-2：再FB

--- script1.js
+++ script1.js
@@ -1,46 +1,47 @@
-$(function(){
+$(function () {
 
-	$('input[name="check"]').on('click', function() {
+	$('input[name="check"]').on('click', function () {
 		var isbn = $('input[name="isbn"]').val();
-		if(checkISBN(isbn)){
+		if (checkISBN(isbn)) {
 			$('.result').removeClass('error').text('正しいISBNコードです。');
 
-		}else{
+		} else {
 			$('.result').addClass('error').text('不正なISBNコードです。');
 		}
 	});
 
 	function checkISBN(isbn) {
-		//ハイフン有無の判定
-		var regExpHyphen = /^(ISBN)?\d+-?\d-?\d+-?\d+-?\d$/i;
-		if (!regExpHyphen.test(isbn)) {
+		var splitIsbn = isbn.replace(/ISBN|-/gi, ''), // ISBN,ハイフンをすべて除却
+			n = splitIsbn.length,
+			prmCheckDig = 0,
+			numCheckDig;
+
+		//ISBN、ハイフン表記の判定
+		if (!/^(ISBN)?\d+-?\d-?\d+-?\d+-?[0-9X]$/i.test(isbn)) {
 			return false;
 		}
-		//入力値にISBN,ハイフンがある場合除却
-		var splitIsbn = isbn.replace(/ISBN|-/gi, ''); // ISBN,ハイフンをすべて除却
-		var regExpHyphen2 = /^978[014]\d{9}$/;
-		if (!regExpHyphen2.test(splitIsbn)) {
-			return false;
+
+		//チェックディジット算出のパラメータを設定
+		if (/^978[014]\d{9}$/.test(splitIsbn)) { //ISBN-13候補の場合
+			for (var i = 0; i < n - 1; i++) {
+				prmCheckDig += splitIsbn[i] * (i % 2 ? 3 : 1);
+			}
+
+			numCheckDig = 10 - prmCheckDig % 10; //チェックディジット算出
+
+			if (numCheckDig === 10) {
+				numCheckDig = 0;
+			}
+
+			//チェックディジット判定
+			if (numCheckDig != splitIsbn[n - 1]) { //チェックディジットと入力値の末尾の値比較
+				return false;
+			} else {
+				return true;
+			}
 		}
 
-		//チェックディジット計算
-		var n = splitIsbn.length,
-			numD,
-			result = 0;
-
-		for (var i = 0; i < n-1; i++) {
-			i % 2 !== 0 ? numD = 3 : numD = 1;
-
-			result+= splitIsbn[i]*numD;
-		}
-		var checkDigitC = 10 - result % 10;
-
-		if (checkDigitC === 10) {
-			checkDigitC = 0;
-		}
-		if (checkDigitC != splitIsbn[12]) {
-			return false;
-		}
-		return true;
+		return false;
 	}
+
 });
--- script2.js
+++ script2.js
@@ -1,6 +1,6 @@
-$(function() {
+$(function () {
 
-	$('input[name="check"]').on('click', function() {
+	$('input[name="check"]').on('click', function () {
 		var isbn = $('input[name="isbn"]').val();
 		var res = checkISBN(isbn);
 		if (res) {
@@ -17,66 +17,56 @@ $(function() {
 	function checkISBN(isbn) {
 		var splitIsbn = isbn.replace(/ISBN|-/gi, ''), // ISBN,ハイフンをすべて除却
 			n = splitIsbn.length,
-			numResult = 0,
+			prmCheckDig = 0,
 			_res = {},
-			checkDigFlg;
+			mdls,
+			numCheckDig;
 
 		//ISBN、ハイフン表記の判定
 		if (!/^(ISBN)?\d+-?\d-?\d+-?\d+-?[0-9X]$/i.test(isbn)) {
 			return false;
 		}
 
-		if (/^978[014]\d{9}$/.test(splitIsbn)) { //ISBN-13の場合
-			//チェックディジット計算
+		//チェックディジット算出のパラメータを設定
+		if (/^978[014]\d{9}$/.test(splitIsbn)) { //ISBN-13候補の場合
 			for (var i = 0; i < n - 1; i++) {
-				numResult += splitIsbn[i] * (i % 2 ? 3 : 1);
-			}
-			_determinCheckDig(10, numResult, splitIsbn[n - 10], 'ISBN-' + n);
-
-			if (checkDigFlg === 0) {
-				return false;
-			} else {
-				return res;
+				prmCheckDig += splitIsbn[i] * (i % 2 ? 3 : 1);
 			}
 
-		} else if (/^[014]\d{8}[0-9X]$/.test(splitIsbn)) { //ISBN-10の場合
-			//チェックディジット計算
+			mdls = 10;
+
+		} else if (/^[014]\d{8}[0-9X]$/.test(splitIsbn)) { //ISBN-10候補の場合
 			for (var i = 0; i < n - 1; i++) {
-				numResult += splitIsbn[i] * (10 - i);
-			}
-			_determinCheckDig(11, numResult, splitIsbn[n - 10], 'ISBN-' + n);
-
-			if (checkDigFlg === 0) {
-				return false;
-			} else {
-				return res;
+				prmCheckDig += splitIsbn[i] * (10 - i);
 			}
 
-		} else {
-			return false;
+			mdls = 11;
 		}
 
-		//言語、ISBN種類の判定
-		function _determinCheckDig(numA, result, resIsnb, resType) {
-			var numCheckDig = numA - result % numA;
+		//チェックディジット判定
+		if (mdls === 10 || mdls === 11) {
+			numCheckDig = mdls - prmCheckDig % mdls; //チェックディジット算出
 
-			//10桁のときだけ行う処理
-			if (splitIsbn.length === 10 && numCheckDig === 10) {
+			//10桁の場合→Xの判定
+			if (n === 10 && numCheckDig === 10) {
 				numCheckDig = 'X';
 			}
 
-			if (numCheckDig === numA) {
+			if (numCheckDig === mdls) {
 				numCheckDig = 0;
 			}
-			if (numCheckDig != splitIsbn[splitIsbn.length - 1]) {
-				checkDigFlg = 0; //return false
+
+			//チェックディジット判定
+			if (numCheckDig != splitIsbn[n - 1]) { //チェックディジットと入力値の末尾の値比較
 				return false;
 			} else {
-				checkDigFlg = 1; //return true
-				_res['country'] = resIsnb == 4 ? '日本語圏' : '英語圏';
-				_res['type'] = resType;
+				_res['country'] = splitIsbn[n - 10] == 4 ? '日本語圏' : '英語圏';
+				_res['type'] = 'ISBN-' + n;
 				res = _res;
+				return res;
 			}
 		}
+
+		return false;
 	}
 });
